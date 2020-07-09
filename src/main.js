debd20b002b8803d12f4de57071e8da96be79a4e
hrzyusoff <hrzyusoff@gmail.com>
2020-07-10 00:29:25 +0800

    Update main page styling

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@
 import DefaultLayout from '~/layouts/Default.vue';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
+import '~/assets/style.scss';
 
 export default function (Vue, { router, head, isClient }) {
+  // Import buefy
   Vue.use(Buefy);
 
   // Set default layout as a global component
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,47 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <div class="container">
+      <div class="content-wrapper">
+        <div class="title-block">
+          <h1 class="has-text-weight-bold">
+            "Rome tidak terbina dalam masa satu hari, begitu juga dengan website ini."
+          </h1>
+        </div>
+      </div>
+      <div class="footer-wrapper">
+        <label class="credit-block">Hak cipta terpelihara © 2020 Hariz Yusoff</label>
+      </div>
+    </div>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+    title: 'hrzyusoff | Personal Website',
+  },
+};
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
+<style lang="scss" scoped>
+.content-wrapper,
+.footer-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+
+.credit-block,
+.title-block {
+  text-align: center;
+}
+
+.content-wrapper {
+  height: 95vh;
+}
+
+.footer-wrapper {
+  height: 5vh;
 }
 </style>
