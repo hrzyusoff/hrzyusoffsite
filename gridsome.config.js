1e9c38f2f8de46b8552129f35617953e79ff9d89
hrzyusoff <hrzyusoff@gmail.com>
2020-07-19 17:36:28 +0800

    Rename site name

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'hrzyusoff | Personal Website',
+  siteName: 'Hariz Yusoff | Personal Website',
   plugins: [
     {
       use: '@gridsome/plugin-google-analytics',
